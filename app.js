cf3f7c43ae3eaa89fb80ea345c66492c917defd7
Mehmet Emin Avcıl <mehmeteminavcill@gmail.com>
2022-01-12 02:12:11 +0300

    js update

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 const darkBtn = document.querySelector("#dark-btn");
 
-darkBtn.addEventListener("change", function() {
-    if (this.checked) {
-        document.body.classList.toggle("dark-mode");
-        console.log("Checkbox is checked..");
-    } else {
-        console.log("Checkbox is not checked..");
-    }
+darkBtn.addEventListener("click", function() {
+    document.body.classList.toggle("dark-mode");
 });
